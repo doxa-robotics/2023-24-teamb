c9e14e9e90a1acc91e450bf26c9a30b8925bf59d
Spaceyhula <150232198+Spaceyhula@users.noreply.github.com>
2024-01-10 07:30:17 +0000

    Added action on autonomous offense

--- src/main.py
+++ src/main.py
@@ -94,8 +94,12 @@ def autonomous_offense1():
     drivetrain.drive_for(FORWARD, 1000, DistanceUnits.MM,
                          units_v=VelocityUnits.PERCENT, velocity=100,
                          )
+    drivetrain.turn_for(RIGHT, 90, DEGREES)
+    drivetrain.drive_for(FORWARD, 350, DistanceUnits.MM,
+                         units_v=VelocityUnits.PERCENT, velocity=100
+                         )
     drivetrain.turn_for(LEFT, 90, DEGREES)
-    drivetrain.drive_for(FORWARD, 500, DistanceUnits.MM,
+    drivetrain.drive_for(FORWARD, 1000, DistanceUnits.MM,
                          units_v=VelocityUnits.PERCENT, velocity=100
                          )
     drivetrain.turn_for(LEFT, 90, DEGREES)
